3c1f22d5a37967ee19af81406e2fb0cdd3a6b906
jason.omemu <jasonjasyno50@gmail.com>
2019-05-30 23:52:53 +0100

    feat: added invalid sigin handling

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,22 +1,36 @@
 import React, {Component} from 'react';
+import './SignIn.css';
 
 class SignIn extends Component {
     constructor(props) {
         super(props);
         this.state = {
             signInEmail: '',
-            signInPassword: ''
+            signInPassword: '',
+            signInCard: {},
+            errMsg: {}
         }
     }
 
+    /**
+    * @description Updates the state of the signInEmail with user-entered text
+    * @param {object} event - keystroke event
+    */
     onEmailChange = (event) => {
         this.setState({signInEmail: event.target.value})
     }
 
+    /**
+    * @description Updates the state of the signInPassword with user-entered text
+    * @param {object} event - keystroke event
+    */
     onPasswordChange = (event) => {
         this.setState({signInPassword: event.target.value})
     }
 
+    /**
+    * @description Posts a request with the signin info entered by the user to the server
+    */
     onSignIn = () => {
         fetch('https://peaceful-stream-83121.herokuapp.com/signin', {
             method: 'post',
@@ -27,10 +41,22 @@ class SignIn extends Component {
             })
         })
         .then(response => response.json())
+        // Checks if the fields have information entered correctly and handles them accordingly
         .then(user => {
             if (user.id) {
                 this.props.loadUser(user);
                 this.props.onRouteChange('home');
+            }
+            else {
+                this.setState({
+                    signInCard: document.getElementById('signInCard'),
+                    errMsg: document.getElementById('err-msg')
+                });
+                this.state.signInCard.classList.add('wrongInfoAnimation');
+                this.state.errMsg.classList.toggle('invisible');
+                this.state.errMsg.classList.toggle('visible');
+                // Removes the animation class just after the animation is completed so it can be added again
+                setTimeout(() => this.state.signInCard.classList.remove('wrongInfoAnimation'), 1300);
             }
         })
     }
@@ -38,11 +64,12 @@ class SignIn extends Component {
     render() {
         return (
             <div>
-                <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+                <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center" id="signInCard">
                     <main className="pa4 black-80">
                         <div className="measure center">
                             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                                 <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                                <label className="fw6 lh-copy f6 invisible" id="err-msg">You have not entered a username and/or password</label>
                                 <div className="mt3">
                                     <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                                     <input
@@ -78,10 +105,6 @@ class SignIn extends Component {
                         </div>
                     </main>
                 </article>
-
-                <div className='wrong-details_modal'>
-                    <h1>Try Again</h1>
-                </div>
             </div>
         );
     }
